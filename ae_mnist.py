79afb2894b9135b60570321d5ef119b99b5f5091
salty-vanilla <jacketgardensio@gmail.com>
2017-07-17 00:46:07 +0900

    add layers.py and fix ae_mnist.py

--- ae_mnist.py
+++ ae_mnist.py
@@ -6,28 +6,12 @@ import gzip
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+from layers import dense
+
 try:
     import pickle
 except:
     import cPickle as pickle
-
-
-def dense(input_, input_dim, output_dim, activation='linear', name=None):
-    if name is None:
-        name = 'dense_{}'.format(output_dim)
-    with tf.variable_scope(name):
-        weights = tf.Variable(
-            tf.truncated_normal([input_dim, output_dim],
-                                stddev=1.0 / math.sqrt(float(input_dim))),
-            name=name+'weights')
-        biases = tf.Variable(tf.zeros([output_dim]), name=name+'biases')
-
-        if activation == 'relu' or 'ReLU':
-            return tf.nn.relu(tf.matmul(input_, weights) + biases, name='relu')
-        elif activation == 'sigmoid':
-            return tf.sigmoid(tf.matmul(input_, weights) + biases, name='sigmoid')
-        elif activation == 'linear' or 'Linear' or 'LINEAR':
-            return tf.matmul(input_, weights) + biases
 
 
 class Autoencoder:
